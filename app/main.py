e4291997d16489236bc33fd5fa520c754dea67d4
SaiBalajiJammu <saibalaji4013@gmail.com>
2026-05-15 20:49:29 +0530

    Added CORS middleware

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
